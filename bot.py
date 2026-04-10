9c95d0c33580c5bead98922e3f50db456348f78e
Sergio970 <46823576+SergioVillasenor970@users.noreply.github.com>
2026-04-11 00:25:46 +0200

    Added audio control functions

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 '''
 TODO:
-* Cambiar ejecutable ffmpeg
 * Cambiar ejecutable yt-dlp
 '''
 
@@ -31,62 +30,115 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+
+async def test(message):
+    await message.channel.send("ACK")
+    return
+
+async def join(message):
+    # -------
+    voice_channel = message.author.voice.channel # Canal de voz del usuario
+    voice_client = message.guild.voice_client # Cliente de voz del bot
+    say = message.channel.send # Enviar mensaje
+    # -------
+
+    if not message.author.voice or not voice_channel:
+        await say("No estas en un canal de voz.")
+        return
+
+    voice_channel = message.author.voice.channel
+    if voice_client:
+        await voice_client.move_to(voice_channel)
+    else:
+        await voice_channel.connect()
+    await say(f"Entrando a {voice_channel.name}.")
+    return
+
+async def disconnect(message):
+    # -------
+    voice_client = message.guild.voice_client # Cliente de voz del bot
+    say = message.channel.send # Enviar mensaje
+    # -------
+
+    if voice_client:
+        await voice_client.disconnect()
+        await say("Desconectado del canal.")
+        voice_client.cleanup()
+    else:
+        await say("No estoy en ningún canal de voz.")
+    return
+
+async def play(message):
+    # -------
+    voice_channel = message.author.voice.channel # Canal de voz del usuario
+    voice_client = message.guild.voice_client # Cliente de voz del bot
+    say = message.channel.send # Enviar mensaje
+    # -------
+    raw_path = os.path.join(BASE_DIR, "audio", "Parklife.webm")
+    if not message.author.voice or not message.author.voice.channel:
+        await say("No estás en un canal de voz.")
+        return
+
     
+    if not voice_client:
+        voice_client = await voice_channel.connect()
+    elif voice_client.channel != voice_channel:
+        await voice_client.move_to(voice_channel)
+
+
+    if voice_client.is_playing():
+            voice_client.pause()
+    audio = discord.FFmpegPCMAudio(raw_path)
+    voice_client.play(audio)
+    await say("Reproduciendo.")
+    return
+
+async def pause(message):
+    # -------
+    voice_client = message.guild.voice_client # Cliente de voz del bot
+    # -------
+    if voice_client.is_playing():
+            voice_client.pause()
+    return
+
+async def resume(message):
+    # -------
+    voice_client = message.guild.voice_client # Cliente de voz del bot
+    # -------
+    if voice_client.is_paused():
+            voice_client.resume()
+    return
+
 
 @client.event
 async def on_message(message):
+
+
+# Código de seguridad para que el bot no interprete sus propios mensajes
     if message.author == client.user:
         return
-    i = 0
-    lista = list(range(5))
 
     content = message.content.strip().lower()
 
+# Bloques de acciones por comandos
     if content == 'test':
-        i += 1
-        await message.channel.send("ACK")
+        await test(message)
 
     if content == 'kjoin':
-        if not message.author.voice or not message.author.voice.channel:
-            await message.channel.send("No estas en un canal de voz.")
-            return
-
-        voice_channel = message.author.voice.channel
-        if message.guild.voice_client:
-            await message.guild.voice_client.move_to(voice_channel)
-        else:
-            await voice_channel.connect()
-        await message.channel.send(f"Entrando a {voice_channel.name}.")
+        await join(message)
+    
+    if content == 'kdc' or content == "kdisconnect":
+        await disconnect(message)
 
     if content.startswith('kplay'):
-        '''
-        raw_path = message.content[5:].strip().strip('"')
-        if not raw_path:
-            await message.channel.send("Ruta vacia.")
-            return
-        if not os.path.isfile(raw_path):
-            await message.channel.send("No encuentro ese archivo.")
-            return
-        '''
-        raw_path = os.path.join(BASE_DIR, "audio", "Parklife.webm")
-        if not message.author.voice or not message.author.voice.channel:
-            await message.channel.send("No estás en un canal de voz.")
-            return
+        await play(message)
+    
+    if content == ('kpause'):
+        await pause(message)
 
-        voice_channel = message.author.voice.channel
-        voice_client = message.guild.voice_client
-        if not voice_client:
-            voice_client = await voice_channel.connect()
-        elif voice_client.channel != voice_channel:
-            await voice_client.move_to(voice_channel)
+    if content == ('kresume'):
+        await resume(message)
 
-        if voice_client.is_playing():
-            #voice_client.stop()
-            voice_client.pause()
-
-        audio = discord.FFmpegPCMAudio(raw_path)
-        voice_client.play(audio)
-        await message.channel.send("Reproduciendo.")
         
 
 client.run(TOKEN)
